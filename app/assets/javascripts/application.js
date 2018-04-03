e5e042293d35dfe801ddbd1fc4bcfe408a533437
Ghislain Chavanne <gh.chavanne@gmail.com>
2018-04-03 10:02:05 +0200

    advanced

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,26 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require_tree .
+
+$( document ).ready(function() {
+  $(document).on('click', '#btn', function(event){
+      event.preventDefault();
+      var offset = - 0;
+
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top + offset
+      }, 500);
+  });
+});
+
+$( document ).ready(function() {
+  $(document).on('click', '#btn-banner', function(event){
+      event.preventDefault();
+      var offset = - 0;
+
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top + offset
+      }, 500);
+  });
+});
