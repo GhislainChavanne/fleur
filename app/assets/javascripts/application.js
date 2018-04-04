a718cd10a099aa80190c169f0f5f0dcfaa32b230
Ghislain Chavanne <gh.chavanne@gmail.com>
2018-04-04 16:34:58 +0200

    better front

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,18 @@
 $( document ).ready(function() {
   $(document).on('click', '#btn', function(event){
       event.preventDefault();
-      var offset = - 210;
+      var offset = - 170;
+
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top + offset
+      }, 500);
+  });
+});
+
+$( document ).ready(function() {
+  $(document).on('click', '#btn2', function(event){
+      event.preventDefault();
+      var offset = - 200;
 
       $('html, body').animate({
           scrollTop: $( $.attr(this, 'href') ).offset().top + offset
